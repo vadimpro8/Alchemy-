9ad0228a035dac2eef7183ed1d6f07ca859aa90e
vadimpro8 <vadim.khoroshev@gmail.com>
2026-03-14 13:42:53 +0700

    Update route.ts

--- Alchemy web/app/api/submit-application/route.ts
+++ Alchemy web/app/api/submit-application/route.ts
@@ -1,7 +1,8 @@
 import { NextResponse } from 'next/server';
 import { prisma } from '@/lib/prisma';
 
-export const dynamic = 'force-dynamic';
+export const runtime = "nodejs";
+export const dynamic = "force-dynamic";
 
 export async function POST(request: Request) {
   try {
